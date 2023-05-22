9bd2f8a47af2677742c3e6e16c86a511d8686e66
Firdaus Bagus <120537868+FirdausBagus02@users.noreply.github.com>
2023-05-22 09:22:10 +0700

    Update archive_delete_postgre.py

--- PostgreSQL/Archive and delete/archive_delete_postgre.py
+++ PostgreSQL/Archive and delete/archive_delete_postgre.py
@@ -1,1 +1,43 @@
+import psycopg2
+import csv
+import os
+from datetime import datetime, timedelta
 
+conn = psycopg2.connect(
+    host="your-hostname",
+    database="your-database",
+    user="your-username",
+    password="your-password"
+)
+
+days_to_keep = 210
+time_delta = datetime.now() - timedelta(days=days_to_keep)
+
+cur = conn.cursor()
+export_query = "SELECT * FROM your_table WHERE date_trunc('day', timestamp) < %s"
+
+cur.execute(export_query, (time_delta.date(),))
+
+results = cur.fetchall()
+colnames = [desc[0] for desc in cur.description]
+
+for row in results:
+    row_date = row[0].strftime('%Y-%m-%d')
+    filename = f'{row_date}.csv'
+    folder_name = 'extracted_data'  
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    filename = os.path.join(folder_name, filename)
+    with open(filename, 'a', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(row)
+
+conn.commit()
+delete_query = "DELETE FROM your_table WHERE date_trunc('day', timestamp) < %s"
+cur.execute(delete_query, (time_delta.date(),))
+conn.commit()
+
+cur.close()
+conn.close()
+
+print("Data extraction and deletion completed.")
